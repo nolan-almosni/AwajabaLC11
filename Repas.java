226998873431b5f32df845eca253655bafac83b8
nolan.almosni.sio@gmail.com <nolan.almosni.sio@gmail.com>
2021-09-15 15:40:54 +0200

    Classe Repas, declaration constructeur

--- Repas.java
+++ Repas.java
@@ -3,5 +3,9 @@ class Repas{
     private int numero;
     private LocalDate date;
 
+    public Repas(int numero, LocalDate date){
+        this.numero = numero;
+        this.date = date;
+    }
 
 }
